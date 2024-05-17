7c51e3ef92641422140fa57b93b691ca39714a8f
AulakhKamal <kau16039@sheridancollege.ca>
2024-05-17 10:16:30 -0400

    kamal

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -13,6 +13,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * push operation
  * showing change on github
  * changing the file on github
+ Kamal
  * @author dancye May 7, 2018
  */
 public class Student 
